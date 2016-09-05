76a1996ef0e9b22a41f403a356838785e9163226
Hiroki Kumamoto <hiroki.kumamoto@access-company.com>
2016-09-05 10:57:01 +0900

    Add unittest task

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,16 @@
 
 var gulp   = require('gulp');
 var eslint = require('gulp-eslint');
+var mocha  = require('gulp-mocha');
 
 gulp.task('eslint', function() {
   return gulp.src(['src/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', function() {
+  gulp.src(['./test/**/*.js'], {read: false})
+    .pipe(mocha({reporter: 'list'}));
 });
