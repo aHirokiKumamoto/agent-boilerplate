c15e793735a6b6f6f277d072473b72f5f4499947
Hiroki Kumamoto <hiroki.kumamoto@access-company.com>
2016-09-05 10:48:00 +0900

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+/* eslint-env node, script */
+/* eslint "strict": "off" */
+
+'use strict';
+
+var gulp   = require('gulp');
+var eslint = require('gulp-eslint');
+
+gulp.task('eslint', function() {
+  return gulp.src(['src/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
